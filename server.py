795a31578694c2852cd059b7f9a5938f9dc8431a
dimavologzh <dima0996@gmail.com>
2025-07-21 20:07:57 -0400

    Task 8: Run static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+""" This project uses the Emotion Predict function of the IBM Watson NLP Library
+    for a text emotion detection.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,17 +8,18 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """ Function to detect emotions """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
     items = list(response.items())
-    
+
     # Check if the first value is None, indicating an error or invalid input
     if items[-1][1] is None:
         output = "Invalid text! Please try again!"
-    else:    
+    else:
         # Extract all items except the last one
         emotion_items = items[:-1]  # all except 'dominant_emotion'
         dominant = items[-1][1]     # value of 'dominant_emotion'
@@ -34,14 +38,13 @@ def sent_analyzer():
         output += f"The dominant emotion is {dominant}."
 
         # Return a formatted string with the emotions labels and scores
-    
-    return output
 
+    return output
 
 @app.route("/")
 def render_index_page():
+    """ Function to render the Web App main page """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
